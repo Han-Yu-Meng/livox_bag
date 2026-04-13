cdb8b2453dff6140223b8df65a8398c6dcd8d0b5
Han-Yu-Meng <steven_mhy@outlook.com>
2026-04-13 14:52:30 +0800

    feat: 更新到最新 api

--- src/nodes.cpp
+++ src/nodes.cpp
@@ -1,3 +1,11 @@
+/*******************************************************************************
+ * Copyright (c) 2025.
+ * IWIN-FINS Lab, Shanghai Jiao Tong University, Shanghai, China.
+ * All rights reserved.
+ ******************************************************************************/
+
+// nodes.hpp
+
 #include <fins/node.hpp>
 #include <livox_driver2/msg/custom_msg.hpp>
 #include <sensor_msgs/msg/imu.hpp>
@@ -57,38 +65,38 @@ private:
     }
   }
 
-  void on_livox(const fins::Msg<livox_driver2::msg::CustomMsg>& msg) {
+  void on_livox(const livox_driver2::msg::CustomMsg& msg) {
     if (!ofs_.is_open()) return;
     
     std::lock_guard<std::mutex> lock(mtx_);
     uint8_t type = 0;
-    uint64_t ts = static_cast<uint64_t>(msg->header.stamp.sec) * 1000000000ULL + msg->header.stamp.nanosec;
+    uint64_t ts = static_cast<uint64_t>(msg.header.stamp.sec) * 1000000000ULL + msg.header.stamp.nanosec;
     
     ofs_.write(reinterpret_cast<const char*>(&type), sizeof(type));
     ofs_.write(reinterpret_cast<const char*>(&ts), sizeof(ts));
     
-    uint32_t point_num = msg->point_num;
-    uint32_t fixed_size = sizeof(msg->timebase) + sizeof(msg->point_num) + sizeof(msg->lidar_id) + sizeof(msg->rsvd);
+    uint32_t point_num = msg.point_num;
+    uint32_t fixed_size = sizeof(msg.timebase) + sizeof(msg.point_num) + sizeof(msg.lidar_id) + sizeof(msg.rsvd);
     uint32_t points_size = point_num * sizeof(livox_driver2::msg::CustomPoint);
     uint32_t total_data_size = fixed_size + points_size;
     
     ofs_.write(reinterpret_cast<const char*>(&total_data_size), sizeof(total_data_size));
-    ofs_.write(reinterpret_cast<const char*>(&msg->timebase), sizeof(msg->timebase));
-    ofs_.write(reinterpret_cast<const char*>(&msg->point_num), sizeof(msg->point_num));
-    ofs_.write(reinterpret_cast<const char*>(&msg->lidar_id), sizeof(msg->lidar_id));
-    ofs_.write(reinterpret_cast<const char*>(&msg->rsvd), sizeof(msg->rsvd));
+    ofs_.write(reinterpret_cast<const char*>(&msg.timebase), sizeof(msg.timebase));
+    ofs_.write(reinterpret_cast<const char*>(&msg.point_num), sizeof(msg.point_num));
+    ofs_.write(reinterpret_cast<const char*>(&msg.lidar_id), sizeof(msg.lidar_id));
+    ofs_.write(reinterpret_cast<const char*>(&msg.rsvd), sizeof(msg.rsvd));
     
     if (point_num > 0) {
-      ofs_.write(reinterpret_cast<const char*>(msg->points.data()), points_size);
+      ofs_.write(reinterpret_cast<const char*>(msg.points.data()), points_size);
     }
   }
 
-  void on_imu(const fins::Msg<sensor_msgs::msg::Imu>& msg) {
+  void on_imu(const sensor_msgs::msg::Imu& msg) {
     if (!ofs_.is_open()) return;
     
     std::lock_guard<std::mutex> lock(mtx_);
     uint8_t type = 1;
-    uint64_t ts = static_cast<uint64_t>(msg->header.stamp.sec) * 1000000000ULL + msg->header.stamp.nanosec;
+    uint64_t ts = static_cast<uint64_t>(msg.header.stamp.sec) * 1000000000ULL + msg.header.stamp.nanosec;
     
     struct PackedImu {
         double orientation[4];
@@ -96,16 +104,16 @@ private:
         double linear_acceleration[3];
     } packed;
     
-    packed.orientation[0] = msg->orientation.x;
-    packed.orientation[1] = msg->orientation.y;
-    packed.orientation[2] = msg->orientation.z;
-    packed.orientation[3] = msg->orientation.w;
-    packed.angular_velocity[0] = msg->angular_velocity.x;
-    packed.angular_velocity[1] = msg->angular_velocity.y;
-    packed.angular_velocity[2] = msg->angular_velocity.z;
-    packed.linear_acceleration[0] = msg->linear_acceleration.x;
-    packed.linear_acceleration[1] = msg->linear_acceleration.y;
-    packed.linear_acceleration[2] = msg->linear_acceleration.z;
+    packed.orientation[0] = msg.orientation.x;
+    packed.orientation[1] = msg.orientation.y;
+    packed.orientation[2] = msg.orientation.z;
+    packed.orientation[3] = msg.orientation.w;
+    packed.angular_velocity[0] = msg.angular_velocity.x;
+    packed.angular_velocity[1] = msg.angular_velocity.y;
+    packed.angular_velocity[2] = msg.angular_velocity.z;
+    packed.linear_acceleration[0] = msg.linear_acceleration.x;
+    packed.linear_acceleration[1] = msg.linear_acceleration.y;
+    packed.linear_acceleration[2] = msg.linear_acceleration.z;
 
     uint32_t size = sizeof(packed);
     ofs_.write(reinterpret_cast<const char*>(&type), sizeof(type));
@@ -178,7 +186,7 @@ public:
       msg.points.resize(msg.point_num);
       ifs_.read(reinterpret_cast<char*>(msg.points.data()), msg.point_num * sizeof(livox_driver2::msg::CustomPoint));
       
-      send("lidar", msg, fins::now());
+      send("lidar", msg);
     } else if (type == 1) { // IMU
       sensor_msgs::msg::Imu msg;
       msg.header.stamp.sec = ts / 1000000000ULL;
@@ -203,7 +211,7 @@ public:
       msg.linear_acceleration.y = packed.linear_acceleration[1];
       msg.linear_acceleration.z = packed.linear_acceleration[2];
 
-      send("imu", msg, fins::now());
+      send("imu", msg);
     }
   }
 
